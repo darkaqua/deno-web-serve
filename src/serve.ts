de0501078bf3cbb86ade0801a0a35b016eec8b39
Pablo González <pagoru@gmail.com>
2023-03-26 21:13:53 +0200

    Fixes

--- src/serve.ts
+++ src/serve.ts
@@ -2,31 +2,17 @@ import { serve } from '$deno/http/server.ts';
 import * as path from "$deno/path/mod.ts";
 import {getCurrentFilePath} from "./utils.ts";
 
-const watchPathProcess = async () => {
-	const process = Deno.run({
+const watchPathProcess = () => {
+	Deno.run({
 		cmd: ['deno', 'run', '-A', '--unstable', '--watch=src/', getCurrentFilePath('watcher.ts')],
 		stdout: 'piped',
 		stderr: 'piped',
 	});
-	
-	const [, , stderr] = await Promise.all([
-		process.status(),
-		process.output(),
-		process.stderrOutput(),
-	]);
-	
-	const error = new TextDecoder().decode(stderr);
-	error ? console.error(error) : console.log(`Done!`);
-	
-	process.close();
 }
 
 export const webServe = async () => {
 	const currentPublicPath = path.join(await Deno.cwd(), 'public/')
 	const isDevelopment = Deno.env.get('ENVIRONMENT')! === 'DEVELOPMENT';
-
-	if(isDevelopment)
-		watchPathProcess();
 	
 	const ENVIRONMENT_LIST = ["ENVIRONMENT", "API_URL"];
 	const environmentsJson = JSON.stringify(ENVIRONMENT_LIST
@@ -62,32 +48,24 @@ export const webServe = async () => {
 			} catch (e) {
 				targetChecksum = 'Error';
 				Deno.writeTextFileSync(currentPublicPath + 'bundle.js', 'test');
-			} finally {
-				if (lastChecksum && lastChecksum !== targetChecksum) {
-					socketList.forEach(
-						(ws?: WebSocket) =>
-							ws && ws?.readyState === WebSocket.OPEN && ws?.send('reload')
-					);
-					lastChecksum = targetChecksum;
-				}
 			}
-		}, 100);
+			if (lastChecksum && lastChecksum !== targetChecksum) {
+				const socketClientList = socketList.filter((ws?: WebSocket) => ws && ws?.readyState === WebSocket.OPEN)
+				console.log(`Sending changes to clients (${socketClientList.length})`)
+				socketClientList.forEach((ws: WebSocket) => ws.send('reload'));
+			}
+			lastChecksum = targetChecksum;
+		}, 20);
 
 		const onRequestWebSocket = (request: Request) => {
 			if (request.headers.get('upgrade') === 'websocket') {
 				const { socket: ws, response } = Deno.upgradeWebSocket(request);
-
-				const indexPos = socketList.push(ws);
-
-				ws.onclose = () => {
-					socketList[indexPos] = undefined;
-				};
-
+				socketList.push(ws);
 				return response;
 			}
 		};
 
-		const onRequestIndex = async (): Promise<Response> => {
+		const onRequestIndex = (): Response => {
 			const indexText = indexFileText.replace(
 				/<!-- SCRIPT_FOOTER -->/,
 				`<script type="text/javascript">\n${developmentHotRefresh}</script>`
@@ -104,7 +82,11 @@ export const webServe = async () => {
 			onRequestIndex,
 		};
 	})();
-
+	
+	
+	if(isDevelopment)
+		watchPathProcess();
+	
 	serve(
 		async (request: Request) => {
 			const webSocketResponse = DevelopmentFunctions?.onRequestWebSocket(request);
@@ -127,7 +109,7 @@ export const webServe = async () => {
 					return new Response('404 Not Found', { status: 404 });
 				}
 
-				const devResponse = await DevelopmentFunctions?.onRequestIndex();
+				const devResponse = DevelopmentFunctions?.onRequestIndex();
 				if (devResponse) return devResponse;
 
 				return new Response(indexFileText, {
