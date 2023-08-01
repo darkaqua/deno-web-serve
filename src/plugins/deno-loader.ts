8fe52f0ef73080d5c34430abda926d996f12fdf1
Alberto Quiles Boix <alberto.quil3s@gmail.com>
2023-08-02 01:15:01 +0200

    feat(plugins/deno): add deno loader plugin

--- src/plugins/deno-loader.ts
+++ src/plugins/deno-loader.ts
@@ -2,34 +2,38 @@
 export const denoLoaderPlugin = () => {
   const externalsCode: Record<string, Uint8Array> = {}
 
-  return {
-    name: "deno-loader",
-    setup(build: any) {
-      build.onResolve({ filter: /^https?:\/\// }, (args: any) => {
-        try {
-          const command = new Deno.Command(Deno.execPath(), {
-            args: ["bundle", args.path],
-          });
-
-          const { stdout } = command.outputSync();
-          externalsCode[args.path] = stdout
-          return {
-            path: args.path,
-            namespace: "deno-loader"
-          }
-        } catch (e) {
-          console.error(`Error descargando "${args.path}": ${e.message}`);
-          return null;
-        }
+  const onResolve = (args) => {
+    try {
+      const command = new Deno.Command(Deno.execPath(), {
+        args: ["bundle", args.path],
       });
 
-      build.onLoad({ filter: /.*/, namespace: "deno-loader" }, (args) => {
-        return {
-          contents: externalsCode[args.path],
-          loader: "js",
-        };
-      });
+      const { stdout } = command.outputSync();
+      externalsCode[args.path] = stdout
+      return {
+        path: args.path,
+        namespace: "deno-loader"
+      }
+    } catch (e) {
+      console.error(`Error descargando "${args.path}": ${e.message}`);
+      return null;
     }
+  };
+
+  const onLoad = (args) => {
+    return {
+      contents: externalsCode[args.path],
+      loader: "js",
+    };
   }
 
+  const setup = (build) => {
+    build.onResolve({ filter: /^https?:\/\// }, onResolve);
+    build.onLoad({ filter: /.*/, namespace: "deno-loader" }, onLoad);
+  }
+
+  return {
+    name: "deno-loader",
+    setup,
+  }
 }
